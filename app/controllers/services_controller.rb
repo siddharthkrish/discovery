9e9f53ff91a030122cf9629dc030102a95a08c11
Sid <siddharth.krish@moneysmart.co>
2018-09-04 15:01:04 +0800

    second

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,25 @@
-class ServicesController < ApplicationController
+class ServicesController < ActionController::API
+    # hasmap that will act as an inmemory store
+    
+    # GET /services
+    def index
+    end
+
+    # POST /services
+    def create
+    end
+
+    # GET /services/:id
+    def show
+    end
+
+    # PUT /services/:id
+    def update
+        head :no_content
+    end
+
+    # DELETE /services/:id
+    def destroy
+        head :no_content
+    end
 end
