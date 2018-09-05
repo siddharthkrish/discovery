cee669116680bd4ef9dc9917131ed487cd9dfb01
Sid <siddharth.krish@moneysmart.co>
2018-09-05 10:49:44 +0800

    using hash as cache ;)

--- app/models/Service.rb
+++ app/models/Service.rb
@@ -4,15 +4,15 @@ class Service
   attr_accessor :name, :version, :created_by
 
   def save!
-    @store[name] = self
+    @@store[name] = self
   end
 
   def self.all
-    @store
+    @@store
   end
 
   private
 
-  # hasmap that will act as an inmemory store
-  @store = {}
+  # hasmap that will act as an in-memory store
+  @@store = Hash.new
 end
